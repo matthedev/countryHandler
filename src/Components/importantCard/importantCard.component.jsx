0a5da864a8c5d66b47421188b9fc2ce7ab6cd5f4
matthedev <mtuxiashvili@yahoo.com>
2020-09-21 18:44:27 -0700

    component added

--- src/Components/importantCard/importantCard.component.jsx
+++ src/Components/importantCard/importantCard.component.jsx
@@ -4,18 +4,18 @@ import "./importantCard.styles.css";
 class importantCard extends Component {
   render() {
     if (this.props.country) {
+      console.log(this.props.country.population);
       return (
         <div className={this.props.show ? "iCard iCard-show" : "iCard"}>
           <div className="iCard__flag">
             <img src={this.props.country.flag} alt="geo" />
           </div>
-          <h3>Country</h3>
-          <h3>Language</h3>
-          <h3>Capital</h3>
-          <h3>Population</h3>
-          <h3>Region</h3>
-          <h3>Currency</h3>
-          <h3>cioc</h3>
+          <h3>Country: {this.props.country.name} </h3>
+          <h3>Language: {this.props.country.languages[0].name}</h3>
+          <h3>Capital: {this.props.country.capital}</h3>
+          <h3>Population: {this.props.country.population.toString()} </h3>
+          <h3>Region: {this.props.country.region}</h3>
+          <h3>Currency: {this.props.country.currencies[0].name}</h3>
         </div>
       );
     } else {
